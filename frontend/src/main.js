c877a41262d2f9de5a3da82cbcf932349a5d78d7
Eduardo Frota <eduardofrotati@gmail.com>
2025-08-04 21:49:04 -0300

    Estilizando os componentes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import App from './App.vue'
 import router from './router'
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     vueDevTools(),
   ],
   resolve: {
+    // extensions: ['.js','.json','.vue'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
