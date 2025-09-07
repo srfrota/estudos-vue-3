8ee03ca196450776f6d3727ec68469fc42068103
Eduardo Frota <eduardofrotati@gmail.com>
2025-09-07 10:46:33 -0300

    15. Vue 3 para iniciantes#15 - Listando dados do backend com a options API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,34 @@
-<template></template>
+<template>
+
+<h2>Lista de users</h2>
+
+<ul>
+  <li v-for="user in users" :key="user.id">{{ user.firstName }}</li>
+</ul>
+
+</template>
 
 <script>
+
+import http from '@/services/http.js'
+
 export default {
-  data() {},
+  data() {
+  
+  return{
+    users: [],
+  }
+  },
+
+  async mounted(){
+    try {
+      const {data} = await http.get('http://localhost:8000/api/users');
+      
+      this.users = data;
+      
+    } catch (error) {
+      
+    }
+  }
 };
 </script>
